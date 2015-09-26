bd24ae2a968ec77f9fe575c95606323facf20b51
jaredeinstein <jared.einstein@gmail.com>
2015-09-25 23:38:52 -0400

    profile editable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :already_login, only: [:new]
-  before_action :require_user, only: :show
+  before_action :require_user, only: [:show, :edit]
+  before_action :correct_user,   only: [:edit, :update]
   
   
   def new
@@ -19,9 +20,32 @@ class UsersController < ApplicationController
   def show
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to profile_path
+    else
+      render edit_user_path
+    end
+  end
+  
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
+  
+  # Confirms the correct user.
+    def correct_user
+      begin
+        @user = User.find(params[:id])
+		 rescue ActiveRecord::RecordNotFound => e
+        @user = nil
+		 end
+      redirect_to(edit_user_path(current_user)) unless current_user?(@user)
+    end
   
 end
